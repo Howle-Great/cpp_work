c8c77d3de7e20d1825054918f6fb07d23983984f
Howle <mdv413@mail.ru>
2019-12-18 14:48:26 +0300

    Initial Commit

---
+++ work1_18_12_20/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  work1_18_12_20
+//
+//  Created by Howle on 18.12.2019.
+//  Copyright © 2019 Howle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
